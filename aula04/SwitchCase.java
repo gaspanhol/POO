3447d75072e2ccc4c6579e25477b4419a3c7a08d
GabrielSpanholAbreu <gabriel.a2004@aluno.ifsc.edu.br>
2026-02-24 18:45:33 -0300

    Correção da aula04

--- aula04/SwitchCase.java
+++ aula04/SwitchCase.java
@@ -4,24 +4,22 @@ public class SwitchCase {
     void main(){
         int i=1;
         String s = "";
-        /*
+        
         switch (i) {
             case 1:
                 s = "Um";
-                System.out.println(s);
             break;
 
             default:
                 s = "Outro";
-                System.out.println(s);
             break;
         }
-        */
+        
        
-        String c = switch (i) {
-            case 1 -> "Um";
-            default -> "Outro";
-        };
+        // String c = switch (i) {
+        //     case 1 -> "Um";
+        //     default -> "Outro";
+        // };
 
 
     }
