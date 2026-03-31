37d25e628e614b5747ca76e98def5fcb08c4325c
Gabriel <gabrielskoob@gmail.com>
2026-03-31 11:41:42 -0300

    Finalizado o exercicio 3 do lab 3

--- lab-03/exercicio3/src/main/java/poo/Main.java
+++ lab-03/exercicio3/src/main/java/poo/Main.java
@@ -2,5 +2,25 @@ package poo;
 
 public class Main {
     static void main() {
+        Retangulo r = new Retangulo();
+        IO.println(r.getAltura());
+        IO.println(r.setAltura(5));
+        IO.println(r.getAltura());
+
+        IO.println(r.getLargura());
+        IO.println(r.setLargura(7));
+        IO.println(r.getLargura());
+
+        IO.println(r.getCodif());
+        IO.println(r.setCodif("UTF8"));
+        IO.println(r.getCodif());
+
+        IO.println("Area = " + r.areaRetangulo());
+
+        IO.println("Perimetro = " + r.perimtroRetangulo());
+
+        IO.println(r.toString());
+
+
     }
 }
--- lab-03/exercicio3/src/main/java/poo/Retangulo.java
+++ lab-03/exercicio3/src/main/java/poo/Retangulo.java
@@ -19,5 +19,90 @@ public class Retangulo {
         }
     }
 
-    //TODO fazer os outros 6 passos do exercício 3
+    public Retangulo(){
+        this.largura = 4;
+        this.altura = 3;
+        this.codif = "ASCII";
+    }
+
+    public boolean setAltura(int altura) {
+        if (altura<=0){
+            this.altura = 3;
+            return false;
+        } else {
+            this.altura = altura;
+            return true;
+        }
+    }
+
+    public boolean setLargura(int largura) {
+        if (largura<=0){
+            this.largura = 4;
+            return false;
+        } else {
+            this.largura = largura;
+            return true;
+        }
+    }
+
+    public boolean setCodif(String codif) {
+        if (Objects.equals("ASCII", codif) || Objects.equals("UTF8", codif)){
+            this.codif = codif;
+            return true;
+        } else {
+            this.codif = "ASCII";
+            return false;
+        }
+    }
+
+    public int getAltura(){
+        return altura;
+    }
+
+    public int getLargura() {
+        return largura;
+    }
+
+    public String getCodif() {
+        return codif;
+    }
+
+    public int areaRetangulo(){
+        return this.largura * this.altura;
+    }
+
+    public int perimtroRetangulo(){
+        return 2*(this.largura+this.altura);
+    }
+
+//    public String toString() {
+//        final StringBuilder sb = new StringBuilder("Retangulo{");
+//        sb.append("altura=").append(altura);
+//        sb.append(", largura=").append(largura);
+//        sb.append(", codif='").append(codif).append('\'');
+//        sb.append('}');
+//        return sb.toString();
+//    }
+
+    public String toString(){
+        String cantoEsq= "+";
+        String cantoDir = "+";
+        String meioTopo = "-";
+        String meioCentro = "|";
+        String cantoEsqB = cantoEsq;
+        String cantoDirB = cantoDir;
+        if (Objects.equals("UTF8", codif)) {
+            cantoEsq= "\u250c";
+            cantoDir= "\u2510";
+            meioTopo= "\u2500";
+            meioCentro= "\u2502";
+            cantoEsqB = "\u2514";
+            cantoDirB = "\u2518";
+        }
+        String linhaTopo = cantoEsq + meioTopo.repeat(this.largura - 2) + cantoDir + "\n";
+        String miolo = meioCentro + " ".repeat(this.largura - 2) + meioCentro + "\n";
+        String linhaBase = cantoEsqB + meioTopo.repeat(this.largura - 2) + cantoDirB;
+
+        return linhaTopo + miolo.repeat(this.altura - 2) + linhaBase;
+    }
 }
