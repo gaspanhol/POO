936282f45fff9b4b79cde6f94538371e63f9137e
Gabriel <gabrielskoob@gmail.com>
2026-05-01 21:04:15 -0300

    Finalização dos exercícios da aula 13, adição dos métodos alterarDados e listarPorAno da classe APP. Ajustado também o método cadastrar pra só funcionar se o ISBN não for repetido

--- aula-13/src/main/java/POO/App.java
+++ aula-13/src/main/java/POO/App.java
@@ -19,6 +19,8 @@ public class App {
         this.livros.put("3", c);
         Livro d = new Livro("4","Imperador deus de Duna","Frank",1986);
         this.livros.put("4", d);
+        Livro e = new Livro("5","BlaBlaCar","Jorge",1986);
+        this.livros.put("5", e);
     }
 
     void menu(int opcao){
@@ -34,12 +36,16 @@ public class App {
 
     void cadastrar(){
         var ISBN = IO.readln("Digite o número do ISBN: ");
-        var titulo = IO.readln("Digite o titulo do livro: ");
-        var autor = IO.readln("Digite o autor do livro: ");
-        var ano = Integer.parseInt(IO.readln("Digite o ano de publicação: "));
-        var novoLivro = new Livro(ISBN, titulo, autor, ano);
-        this.livros.put(ISBN, novoLivro);
-        IO.println("Livro adicionado ao acervo");
+        if(livros.containsKey(ISBN)){
+            IO.println("ISBN já cadastrado");
+        } else {
+            var titulo = IO.readln("Digite o titulo do livro: ");
+            var autor = IO.readln("Digite o autor do livro: ");
+            var ano = Integer.parseInt(IO.readln("Digite o ano de publicação: "));
+            var novoLivro = new Livro(ISBN, titulo, autor, ano);
+            this.livros.put(ISBN, novoLivro);
+            IO.println("Livro adicionado ao acervo");
+        }
     }
 
     void listarLivros() {
@@ -50,17 +56,47 @@ public class App {
     }
 
     void listarPorAno(){
-        livros.forEach((ano, valor) -> {
-            IO.println(valor);
-            IO.println("----------------------------------------");
+        int anoEscolhido = Integer.parseInt(IO.readln("Digite o ano para ver a lista de livros publicados nele: "));
+        this.livros.forEach((ISBN, livro) -> {
+            if (livro.getAno() == anoEscolhido) {
+                IO.println(livro);
+                IO.println("----------------------------------------");
+            }
         });
     }
 
     void alterarDados() {
+        var livroEscolhido = IO.readln("Digite o ISBN do livro que você deseja alterar os dados: ");
+        Livro livro = livros.get(livroEscolhido);
+        if (livro == null) {
+            IO.println("Livro não encontrado.");
+            return;
+        } else {
+            IO.println("voce selecionou o livro: " + livro.getTitulo() + "\n");
+        }
+        int opcao = 0;
+        do{
+            IO.println("""
+                    Escolha o dado que deseja alterar
+                    1 - Alterar Titulo do livro
+                    2 - Alterar Autor do livro
+                    3 - Alterar ano de publicação do livro
+                    """);
+            opcao = Integer.parseInt(IO.readln("Entre com uma opção: "));
+        } while (opcao > 3 || opcao < 1);
+        if (opcao == 1) {
+            livro.setTitulo(IO.readln("Entre com o titulo: "));
+        } else if (opcao == 2) {
+            livro.setAutor(IO.readln("Entre com o autor: "));
+        } else {
+            livro.setAno(Integer.parseInt(IO.readln("Entre com o ano: ")));
+        }
 
+        IO.println("Dados alterados com sucesso!");
     }
 
-    static void main() {
+
+    public static void main(String[] args) {
         //Sistema de uma livraria para gestão do acervo da lista
 //        1 - Cadastrar Livro
 //        2 - listar todos os livros
