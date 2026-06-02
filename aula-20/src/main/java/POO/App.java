cd21daf9eb9d15062ea6e1817433f66005f33c89
GabrielSpanholAbreu <gabriel.a2004@aluno.ifsc.edu.br>
2026-06-02 19:59:54 -0300

    Conteúdo de tratamento de excessões através de try catch

--- aula-20/src/main/java/POO/App.java
+++ aula-20/src/main/java/POO/App.java
@@ -2,20 +2,49 @@ package POO;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class App {
     static void main() {
-        Caixa<String> c = new Caixa<>("Olá mundo");
-        Caixa<Pessoa> d = new Caixa<>(new Pessoa ("Joao"));
 
-        // não precisa de typecasting
-        String s = c.getConteudo();
+        // ..:: Conteúdo de tipos genêricos de objetos ::..
+//        Caixa<String> c = new Caixa<>("Olá mundo");
+//        Caixa<Pessoa> d = new Caixa<>(new Pessoa ("Joao"));
+//
+//        // não precisa de typecasting
+//        String s = c.getConteudo();
+//
+//        // erro de compilação, tipos incompatíveis
+//        String errado = d.getConteudo();
+//
+//        ArrayList<String> teste = new ArrayList<>();
+//
+//        HashMap<String,Boolean> testeHash = new HashMap<>();
 
-        // erro de compilação, tipos incompatíveis
-        String errado = d.getConteudo();
+        // ..:: Conteúdo de tratamento de excessão ::..
+        Scanner ler = new Scanner(System.in);
+        int a, b;
 
-        ArrayList<String> teste = new ArrayList<>();
+        try{
+            System.out.println("Entre com o número: ");
+            a = ler.nextInt();
+            System.out.println("Entre com o número: ");
+            b = ler.nextInt();
 
-        HashMap<String,Boolean> testeHash = new HashMap<>();
+            int res = a/b;
+
+            System.out.println(a + " Dividido por " + b + " = " + res);
+        }catch (InputMismatchException e) {
+            System.err.println("Só é permitido números inteiros");
+            ler.nextLine(); // limpa o buffer do teclado
+        }catch (ArithmeticException e) {
+            System.err.println("Não é possível realizar divisões por 0");
+            ler.nextLine();
+        }catch (Exception e) {
+            System.err.println("Comportamento inesperado");
+            ler.nextLine();
+        }
+        System.out.println("Fim do programa");
     }
 }
